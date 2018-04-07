a2930a0cd46d51ca3eae1c73881a8f2401c052b1
Eric Martinez <eric.michael.mtz@gmail.com>
2018-04-07 13:20:23 -0500

    New tests

--- spec/part1_spec.rb
+++ spec/part1_spec.rb
@@ -2,11 +2,7 @@
 require File.expand_path '../spec_helper.rb', __FILE__
 
 describe "My application" do
-  it "should allow accessing the home page [25 points]" do
-    get '/'
-    # Rspec 2.x
-    expect(last_response).to be_ok
-  end
+
 
   it "should say 'Hello World' when home page is accessed with no parameters [25 points]" do
     get '/'
@@ -22,5 +18,19 @@ describe "My application" do
    it "should say 'Hello Felisha' when home page is accessed with parameter name=Felisha [25 points]" do
   	get '/', {"name"=>"Felisha"}
   	expect(last_response.body).to eq("Hello Felisha")
+  end
+
+  it "should allow inputting name into form [25 points]" do
+    visit '/ask_name'
+    fill_in 'name', with: "Herbert"
+
+    form = page.find("form")
+    class << form
+      def submit!
+        Capybara::RackTest::Form.new(driver, native).submit({})
+      end
+    end
+    form.submit!
+    expect(page).to have_content 'Hello Herbert'
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,15 +1,30 @@
 # spec/spec_helper.rb
 require 'rack/test'
 require 'rspec'
-
+require 'capybara'
+require 'capybara/dsl'
 ENV['RACK_ENV'] = 'test'
 
 require File.expand_path '../../web.rb', __FILE__
 
 module RSpecMixin
   include Rack::Test::Methods
+  include Capybara::DSL # Adding this line solved the error
+
   def app() Sinatra::Application end
 end
 
+  class Capybara::Session
+    def submit(element)
+      Capybara::RackTest::Form.new(driver, element.native).submit({})
+    end
+  end
+
+
+Capybara.app = Sinatra::Application
+
 # For RSpec 2.x and 3.x
-RSpec.configure { |c| c.include RSpecMixin }
+RSpec.configure do |c|
+	c.include RSpecMixin 
+	c.include Capybara
+end
